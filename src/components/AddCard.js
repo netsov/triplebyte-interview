2105ca8120a5afa277be5941b7ea466c3e70878a
netsov <a.netsov@gmail.com>
2019-07-06 10:02:18 -0400

    add modal

--- src/components/AddCard.js
+++ src/components/AddCard.js
@@ -5,18 +5,75 @@ import { addCard } from '../redux/actions';
 
 import './AddCard.css';
 
-const AddCard = ({ addCard, column }) => {
-  const add = () => {
-    const cardTitle = prompt('Please enter your card name');
-    if (cardTitle) addCard(cardTitle, column.id);
+class AddCard extends React.Component {
+  state = {
+    modalIsOpen: false,
+    input: ''
   };
-  return (
-    <div>
-      <button className="add-card" onClick={add}>
-        Add Card
-      </button>
-    </div>
-  );
-};
+
+  handleEscape = e => {
+    if (e.key === 'Escape') this.closeModal();
+  };
+
+  openModal = () => {
+    this.setState({ modalIsOpen: true });
+    document.addEventListener('keydown', this.handleEscape);
+  };
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false, input: '' });
+    document.removeEventListener('keydown', this.handleEscape);
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { addCard, column } = this.props;
+    addCard(this.state.input, column.id);
+    this.closeModal();
+  };
+
+  handleChange = e => this.setState({ input: e.target.value });
+
+  renderModal = () => {
+    return (
+      <div className="modal">
+        <div id="result" className="modal-content">
+          <h3>Add Card</h3>
+          <form>
+            <input
+              type="text"
+              placeholder="Please enter your card name"
+              value={this.state.input}
+              onChange={this.handleChange}
+              autoFocus
+            />
+            <div className="actions">
+              <button onClick={this.closeModal}>Cancel</button>
+              <button
+                type="submit"
+                onClick={this.handleSubmit}
+                disabled={!this.state.input}
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  };
+
+  render() {
+    const { modalIsOpen } = this.state;
+    return (
+      <div>
+        <button className="add-card" onClick={this.openModal}>
+          Add Card
+        </button>
+        {modalIsOpen && this.renderModal()}
+      </div>
+    );
+  }
+}
 
 export default connect(null, { addCard })(AddCard);
